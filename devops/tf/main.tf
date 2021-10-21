c07ec01465be69e4dbbd706940abeaac03e56d68
CFE <hello@teamcfe.com>
2021-10-21 15:28:09 -0500

    11 - Perpare for a Linode Virtual Machine

--- devops/tf/main.tf
+++ devops/tf/main.tf
@@ -7,3 +7,18 @@ terraform {
         }
     }
 }
+
+provider "linode" {
+    token = var.linode_pa_token
+}
+
+resource "linode_instance" "cfe-pyapp" {
+    image = "linode/ubuntu18.04"
+    label = "my_first_cfe_pyapp"
+    group = "CFE_Terrafrom_PROJECT"
+    region = var.region
+    type = "g6-nanode-1"
+    authorized_keys = [ var.authorized_key ]
+    root_pass = var.root_user_pw
+    tags = ["python", "docker", "terraform"]
+}
