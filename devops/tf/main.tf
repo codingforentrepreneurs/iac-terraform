7be8be97ce5c7fe00a45ee70187b690d6000768b
CFE <hello@teamcfe.com>
2021-10-22 16:29:55 -0500

    26 - Terraform State via Linode Object Storage

--- devops/tf/main.tf
+++ devops/tf/main.tf
@@ -6,6 +6,10 @@ terraform {
             version = "1.22.0"
         }
     }
+    backend "s3" {
+        skip_credentials_validation = true
+        skip_region_validation = true
+    }
 }
 
 provider "linode" {
